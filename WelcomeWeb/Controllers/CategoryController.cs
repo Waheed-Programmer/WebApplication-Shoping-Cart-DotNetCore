82303e54b799f060821ddd1a751231019a0fb0de
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-07 10:50:17 +0500

    Add new view to show all category

--- WelcomeWeb/Controllers/CategoryController.cs
+++ WelcomeWeb/Controllers/CategoryController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WelcomeWeb.Data;
 
 namespace WelcomeWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _ApplicationDbContext;
+        public CategoryController(ApplicationDbContext Db)
+        {
+            _ApplicationDbContext = Db; 
+        }
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                var list = _ApplicationDbContext.Categories.ToList();
+                return View(list);
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
         }
     }
 }
