90b8efd81f07b3a5b953d8e8a5edd3cdd62e39e6
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-10 08:20:45 +0500

    Apply Repository Pattern

---
+++ MyApp.DataAccessLayer/Infrastructure/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.DataAccessLayer.Infrastructure.IRepository
+{
+    public interface IRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        T GetT(Expression<Func<T, bool>> predicate);
+        void add(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entity);
+    }
+}
