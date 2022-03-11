7322b8e6e241a6ae1f4c431324067cd1e14d342b
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-11 08:58:52 +0500

    Create  a new product controller and its perspective view for show the list of products

---
+++ MyApp.Models/ViewModel/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Models.ViewModel
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; } = new Product();
+        public IEnumerable<Product> products { get; set; } = new List<Product>();
+    }
+}
---
+++ WelcomeWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models;
+using MyApp.Models.ViewModel;
+
+namespace WelcomeWeb.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitofWork _unitofWork;
+        public ProductController(IUnitofWork unitofWork)
+        {
+            _unitofWork = unitofWork; 
+        }
+        public IActionResult Index()
+        {
+            ProductVM Productvm = new ProductVM();
+            Productvm.products = _unitofWork.Product.GetAll();
+           return View(Productvm);
+        }
+               
+        [HttpGet]
+        public IActionResult CreateUpdate(int? Id)
+        {
+            CategoryVM cat = new CategoryVM();
+            if(Id==null || Id == 0)
+            {
+                return View(cat);
+            }
+            else
+            {
+                cat.Category = _unitofWork.Category.GetT(m => m.CategoryId == Id);
+                if (cat.Category == null)
+                {
+                    return NotFound();
+
+                }
+                else
+                {
+                    return View(cat);
+                }
+            }           
+        }
+
+        [HttpPost]
+        public IActionResult CreateUpdate(CategoryVM vm)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (vm.Category.CategoryId == 0)
+                    {
+                        _unitofWork.Category.Add(vm.Category);
+                        TempData["save"] = "Data Save Successfully!";
+
+
+                    }
+                    else
+                    {
+                        _unitofWork.Category.Update(vm.Category);
+                        TempData["edit"] = "Data Update Successfully!";
+
+
+                    }
+                    _unitofWork.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                return View(vm);
+            }
+            return RedirectToAction("Index");   
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var c = _unitofWork.Category.GetT(m=>m.CategoryId == Id);
+            if (c == null)
+            {
+                return NotFound();
+
+            }
+            return View(c);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteData(int? Id)
+        {           
+                var c = _unitofWork.Category.GetT(m=>m.CategoryId==Id);
+
+                if (c==null)
+                {
+                    return NotFound();
+                }
+                _unitofWork.Category.Delete(c);
+                _unitofWork.Save();
+                TempData["delete"] = "Data Delete Successfully!";
+            return RedirectToAction("Index");           
+           
+        }
+
+    }
+}
