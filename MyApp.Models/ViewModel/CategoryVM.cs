cf88cbc2ebed8a19150a3996d71c5bb7e4239d00
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-11 08:41:30 +0500

    Solve model state invalid error

--- MyApp.Models/ViewModel/CategoryVM.cs
+++ MyApp.Models/ViewModel/CategoryVM.cs
@@ -8,7 +8,7 @@ namespace MyApp.Models.ViewModel
 {
     public class CategoryVM
     {
-        public Category Category { get; set; }
-        public IEnumerable<Category> categories { get; set; }   
+        public Category Category { get; set; } = new Category();
+        public IEnumerable<Category> categories { get; set; } = new List<Category>();   
     }
 }
