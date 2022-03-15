4ab4e41e9f4e9f9273fa3e2582411fac678d2406
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-15 10:28:35 +0500

    create a new class for the updates of Payment Status

---
+++ MyApp.CommonHelper/PaymentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.CommonHelper
+{
+    public static class PaymentStatus
+    {
+        public const string  StatusPending = "Pending";       
+        public const string  StatusApproved = "Approved";      
+        public const string  StatusRejected = "Rejected";
+        public const string  StatusDelay = "Delay";
+    }
+}
