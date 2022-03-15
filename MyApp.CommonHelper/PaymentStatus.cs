d5aa05474ea09cc5c9bb5ed57bb31222eb984985
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-15 14:25:59 +0500

    change view model code

--- MyApp.CommonHelper/PaymentStatus.cs
+++ MyApp.CommonHelper/PaymentStatus.cs
@@ -8,9 +8,9 @@ namespace MyApp.CommonHelper
 {
     public static class PaymentStatus
     {
-        public const string  StatusPending = "Pending";       
-        public const string  StatusApproved = "Approved";      
-        public const string  StatusRejected = "Rejected";
-        public const string  StatusDelay = "Delay";
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusRejected = "Rejected";
+        public const string StatusDelay = "Delay";
     }
 }
--- WelcomeWeb/Areas/Customer/Controllers/CartController.cs
+++ WelcomeWeb/Areas/Customer/Controllers/CartController.cs
@@ -35,7 +35,6 @@ namespace WelcomeWeb.Areas.Customer.Controllers
             return View(tCart);
         }
         [Authorize]
-        [HttpGet]
         public IActionResult Summary()
         {
             var claimIdentity = (ClaimsIdentity)User.Identity;
@@ -65,24 +64,34 @@ namespace WelcomeWeb.Areas.Customer.Controllers
         {
             var claimIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            vm.ListOfCart = _unitofWork.Cart.GetAll(x => x.ApplicationUserId == claim.Value, includeProperties: "Product");
-            vm.orderHeader.OrderStatus = OrderStatus.StatusPending;
-            vm.orderHeader.PaymentStatus = PaymentStatus.StatusPending;
-            vm.orderHeader.DateOfOrder = DateTime.Now;
-            vm.orderHeader.ApplicationuserId = claim.Value;
-            foreach (var item in vm.ListOfCart)
+            tCart = new CartVM()
             {
-                vm.orderHeader.OrderTotal += (item.Product.Price * item.Count);
+                ListOfCart = _unitofWork.Cart.GetAll(x => x.ApplicationUserId == claim.Value, includeProperties: "Product"),
+                orderHeader = new MyApp.Models.OrderHeader()
+            };
+            tCart.orderHeader.Applicationuser = _unitofWork.Application.GetT(x => x.Id == claim.Value);
+            tCart.orderHeader.Name = tCart.orderHeader.Applicationuser.Name;
+            tCart.orderHeader.Address = tCart.orderHeader.Applicationuser.Address;
+            tCart.orderHeader.Phon = tCart.orderHeader.Applicationuser.PhoneNumber;
+            tCart.orderHeader.City = tCart.orderHeader.Applicationuser.City;
+            tCart.orderHeader.PostalCode = tCart.orderHeader.Applicationuser.PinCode;
+            tCart.orderHeader.State = tCart.orderHeader.Applicationuser.State;
+            tCart.orderHeader.OrderStatus = OrderStatus.StatusPending;
+            tCart.orderHeader.PaymentStatus = PaymentStatus.StatusPending;
+            tCart.orderHeader.DateOfOrder = DateTime.Now;
+            foreach (var item in tCart.ListOfCart)
+            {
+                tCart.orderHeader.OrderTotal += (item.Product.Price * item.Count);
             }
 
-            _unitofWork.OrderHeader.Add(vm.orderHeader);
+            _unitofWork.OrderHeader.Add(tCart.orderHeader);
             _unitofWork.Save();
-            foreach (var item in vm.ListOfCart)
+            foreach (var item in tCart.ListOfCart)
             {
                 OrderDetail orderDetail = new OrderDetail()
                 {
                     ProductId = item.ProductId,
-                    OrderHeaderId = vm.orderHeader.OrderHeaderId,
+                    OrderHeaderId = tCart.orderHeader.OrderHeaderId,
                     Count = item.Count,
                     Price = item.Product.Price
 
@@ -90,7 +99,7 @@ namespace WelcomeWeb.Areas.Customer.Controllers
                 _unitofWork.OrderDetail.Add(orderDetail);
                 _unitofWork.Save();
             }
-            _unitofWork.Cart.DeleteRange(vm.ListOfCart);
+            _unitofWork.Cart.DeleteRange(tCart.ListOfCart);
             _unitofWork.Save();
             return RedirectToAction("Index","Home");
 
