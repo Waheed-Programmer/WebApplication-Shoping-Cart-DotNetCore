3c1398f6536b2c68a4030f666767f7f1112b601c
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-15 10:25:22 +0500

    create order status flags

---
+++ MyApp.CommonHelper/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.CommonHelper
+{
+    public static class OrderStatus
+    {
+        public const string  StatusPending = "Pending";
+        public const string  StatusRefunded = "Refunded";
+        public const string  StatusApproved = "Approved";
+        public const string  StatusInProcess = "UnderProcessing";
+        public const string  StatusCancelled = "Cancelled";
+        public const string  StatusShipped = "Shipped";
+    }
+}
