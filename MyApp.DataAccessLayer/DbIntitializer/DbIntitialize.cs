c9b86c223b926a852ce945fe001ed3c6e9bf507d
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-16 19:31:38 +0500

    create DbIntitializer Class and interface to implementations of all role in websites

---
+++ MyApp.DataAccessLayer/DbIntitializer/DbIntitialize.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.DataAccessLayer.DbIntitializer
+{
+    public class DbIntitialize : IDbIntitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;
+
+        public DbIntitialize(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager, 
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public void Intitialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
