26a0e7182cd2be4b7464096ee616a4f698096dc6
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-16 08:33:34 +0500

    create a new order controller and its perspective actions method

---
+++ WelcomeWeb/Areas/Admin/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models;
+
+namespace WelcomeWeb.Areas.Admin
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitofWork _unitofWork;
+
+        public OrderController(IUnitofWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region All Order get throug json data
+        public IActionResult AllOrders()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+            orderHeaders = _unitofWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+    }
+}
