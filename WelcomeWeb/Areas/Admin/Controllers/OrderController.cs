0e743e01f142eff7642d90a4b76c2bbee453e531
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-16 16:09:32 +0500

    All code is working mode

--- WelcomeWeb/Areas/Admin/Controllers/OrderController.cs
+++ WelcomeWeb/Areas/Admin/Controllers/OrderController.cs
@@ -37,7 +37,7 @@ namespace WelcomeWeb.Areas.Admin.Controllers
             {
                 var claimIdentity = (ClaimsIdentity)User.Identity;
                 var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
-                orderHeaders = _unitofWork.OrderHeader.GetAll(x=>x.ApplicationuserId==claim.Value);
+                orderHeaders = _unitofWork.OrderHeader.GetAll(x => x.ApplicationuserId == claim.Value);
 
             }
             switch (status)
@@ -68,7 +68,54 @@ namespace WelcomeWeb.Areas.Admin.Controllers
                 OrderDetail = _unitofWork.OrderDetail.GetAll(x => x.Id == OrderID, includeProperties: "Product")
             };
             return View(orderVM);
-        
+
+        }
+        [HttpPost]
+        public IActionResult OrderDetails(OrderVM vM)
+        {
+            var orderHeader = _unitofWork.OrderHeader.GetT(x => x.OrderHeaderId == vM.orderHeader.OrderHeaderId);
+            orderHeader.Name = vM.orderHeader.Name;
+            orderHeader.Address = vM.orderHeader.Address;
+            orderHeader.Phon =  vM.orderHeader.Phon;
+            orderHeader.City = vM.orderHeader.City;
+            orderHeader.State = vM.orderHeader.State;
+            orderHeader.PostalCode = vM.orderHeader.PostalCode;
+            if(vM.orderHeader.Carrior != null)
+            {
+                orderHeader.Carrior = vM.orderHeader.Carrior;
+            }
+            if(vM.orderHeader.TrackingNumber != null)
+            {
+                orderHeader.TrackingNumber = vM.orderHeader.TrackingNumber;
+            }
+            _unitofWork.OrderHeader.Update(orderHeader);
+            _unitofWork.Save();
+            TempData["Save"] = "Information Updated";
+            return RedirectToAction("OrderDetails", "Order", new { id = vM.orderHeader.OrderHeaderId });
+        }
+
+        public IActionResult InProcess(OrderVM vM)
+        {
+            _unitofWork.OrderHeader.UpdateStatus(vM.orderHeader.OrderHeaderId, OrderStatus.StatusInProcess);
+            _unitofWork.Save();
+            TempData["Save"] = "Order status Updated-Inprocess";
+            return RedirectToAction("OrderDetails", "Order", new { id = vM.orderHeader.OrderHeaderId });
+
+        }
+
+        public IActionResult Shipped(OrderVM vM)
+        {
+            var orderHeader = _unitofWork.OrderHeader.GetT(x => x.OrderHeaderId == vM.orderHeader.OrderHeaderId);
+            orderHeader.Carrior = vM.orderHeader.Carrior;
+            orderHeader.TrackingNumber = vM.orderHeader.TrackingNumber;
+            orderHeader.OrderStatus = OrderStatus.StatusShipped;
+            orderHeader.DateOfShipping = DateTime.Now;
+
+            _unitofWork.OrderHeader.Update(orderHeader);
+            _unitofWork.Save();
+            TempData["Save"] = "Order status Updated-Shipped";
+            return RedirectToAction("OrderDetails", "Order", new { id = vM.orderHeader.OrderHeaderId });
+
         }
     }
 }
