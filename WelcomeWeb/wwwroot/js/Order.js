242e9eb52ed843661ac6c28f043a991bd8ae15b7
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-03-16 08:48:27 +0500

    add index view to show orders in admin controller  side

---
+++ WelcomeWeb/wwwroot/js/Order.js
@@ -0,0 +1,27 @@
+﻿var dTable;
+$(document).ready(function () {
+    dTable = $('#myTable').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/AllOrders",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name" },
+            { "data": "orderStatus" },
+            { "data": "phon" },
+            { "data": "orderTotal" },
+            {
+                "data": "OrderHeaderId",
+                "render": function (data) {
+                    return `                    
+                    <a href="/Admin/Order/OrderDetails?id=${data}"><i class="bi bi-pencil-square"></i></a>
+                    <a onclick=DeleteProduct("/Admin/Product/Delete/${data}")><i class="bi bi-trash3-fill"></i></a>
+                       
+                    `
+                }
+            },
+        
+        ]
+    });
+});
